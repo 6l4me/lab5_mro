70e71b61fa3090d4df4dcc33953ac6bc7d6b116a
6l4me <pekarnikov90@mail.ru>
2024-12-09 12:31:30 +0300

    first

--- perceptron.cpp
+++ perceptron.cpp
@@ -25,17 +25,54 @@ int main() {
         C2_x2[i] = C2_x1[i] - (std::rand() % 10) / 10.0 - 0.1;
     }
 
-    // Веса
-    double w[2] = {-0.3, 0.3};
+    // Инициализация весов и параметров обучения
+    double w[2] = {0.0, 0.0}; // Начальные веса
+    double learning_rate = 0.1; // Скорость обучения
+    int epochs = 100; // Количество эпох
+
+    // Обучающие данные
+    std::vector<std::vector<double>> inputs;
+    std::vector<int> labels;
+  // подготовка данных для обучения
+    for (int i = 0; i < N; ++i) {
+        inputs.push_back({C1_x1[i], C1_x2[i]});
+        labels.push_back(1); // Метка класса C1
+
+        inputs.push_back({C2_x1[i], C2_x2[i]});
+        labels.push_back(-1); // Метка класса C2
+    }
+
+    // Процесс обучения
+    // цикл по эпохам
+    for (int epoch = 0; epoch < epochs; ++epoch) {
+      //цикл по данным
+        for (size_t i = 0; i < inputs.size(); ++i) {
+            double x1 = inputs[i][0];
+            double x2 = inputs[i][1];
+            int true_label = labels[i];
+
+            // Вычисляем выход перцептрона
+            double y = w[0] * x1 + w[1] * x2;
+            int predicted_label = (y >= 0) ? 1 : -1;
+
+            // Обновление весов, если предсказание неверное
+            if (predicted_label != true_label) {
+                w[0] += learning_rate * (true_label - predicted_label) * x1;
+                w[1] += learning_rate * (true_label - predicted_label) * x2;
+            }
+        }
+    }
 
     // Открываем файл для записи результатов
     std::ofstream outfile("results.txt");
-    
+
     // Классификация и запись результатов в файл
-    for (int i = 0; i < N; ++i) {
-        double x[2] = {C2_x1[i], C2_x2[i]};
-        double y = w[0] * x[0] + w[1] * x[1];
-        
+    for (size_t i = 0; i < inputs.size(); ++i) {
+        double x1 = inputs[i][0];
+        double x2 = inputs[i][1];
+
+        // Вычисляем выход
+        double y = w[0] * x1 + w[1] * x2;
         if (y >= 0) {
             outfile << "Класс C1" << std::endl;
         } else {
